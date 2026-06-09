b51d5d906da900b459154e81bbf0b89298b7dd46
LRBDeveloper <146123222+LRBDeveloper@users.noreply.github.com>
2026-06-09 10:43:09 +0200

    added Image recognition support for ArUco used for robot detection and goal detection. Added Boundary detection.

--- src/config/config_rules.py
+++ src/config/config_rules.py
@@ -1,28 +1,33 @@
 import numpy as np
+import cv2 as cv
 
 #Color config is for defining the color rules of each object in the picture
 COLOR_CONFIG = {
     "orange_ball": {
         # Orange sits near red's high edge → hue 5-18 works well for a ping-pong ball
-        "lower": np.array([8,  150,  150]),
-        "upper": np.array([18, 255, 255]),
+        "kernel": cv.getStructuringElement(cv.MORPH_RECT, (5,5)),
+        "lower": np.array([12,  60,  160]),
+        "upper": np.array([30, 255, 255]),
         "draw_color": (0, 140, 255),   # BGR – orange for display
     },
     "white_ball": {
         # White = very low saturation, high brightness
+        "kernel": cv.getStructuringElement(cv.MORPH_ELLIPSE, (5,5)),
         "lower": np.array([0,   0, 200]),
         "upper": np.array([180, 40, 255]),
         "draw_color": (200, 200, 200),  # BGR – light gray for display
     },
     "boundary": {
         # Red wraps around 0 in OpenCV HSV; handle both lobes
-        "lower":  np.array([0,   120, 80]),
-        "upper":  np.array([5,   255, 255]),
-        "lower2": np.array([165, 120, 80]),
+        "kernel": cv.getStructuringElement(cv.MORPH_RECT, (2,2)),
+        "lower":  np.array([0,   100, 130]),
+        "upper":  np.array([20,   255, 255]),
+        "lower2": np.array([165, 50, 80]),
         "upper2": np.array([180, 255, 255]),
         "draw_color": (0, 0, 220),      # BGR – red for display
     },
 }
 
 #Doesnt recognize objects small than (in pxs):
-MIN_AREA = 350
+MIN_AREA = 800
+MAX_AREA = 2000
---
+++ src/models/Robot.py
@@ -0,0 +1,18 @@
+class Robot:
+    def __init__(
+        self,
+        x1,
+        y1,
+        x2,
+        y2,
+        rotation,
+        state
+    ):
+        self.collision = {
+            x1,
+            y1,
+            x2,
+            y2,
+        }
+        self.rotation = rotation
+        self.state = state
