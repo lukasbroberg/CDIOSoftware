f1f1ca5c494e5e1bd221d0078077f7908f36a7e4
LRBDeveloper <146123222+LRBDeveloper@users.noreply.github.com>
2026-06-26 11:14:42 +0200

    merged

--- src/models/Ball.py
+++ src/models/Ball.py
@@ -6,4 +6,8 @@ class Ball():
             y: y,
         }
         self.isOrange = isOrange
+        self.size = self.size
     
+    def _repr_(self):
+        color = "Orange" if self.isOrange else "White"
+        return f"Ball({color}, position=({self.position['x']}, {self.position['y']}), size={self.size})"
